196e0321058b8a0d738dcaf0cb9ed76926352155
aedit <udisen97@gmail.com>
2019-03-29 21:46:27 +0530

    Mongo DB Test

---
+++ mongo.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request, json
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from datetime import datetime
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'users'
+app.config['MONGO_URI'] = 'mongodb://vbhv98:hovergames69@ds031257.mlab.com:31257/hover-games'
+app.config['JWT_SECRET_KEY'] = 'secret'
+
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+CORS(app)
+
+
+@app.route('/users/reglog', methods=['POST'])
+def reg():
+    users = mongo.db.users
+    userid = request.get_json()['userid']
+    password = request.get_json()['password']
+    password_bcrypt = bcrypt.generate_password_hash(password).decode('utf-8')
+    created = datetime.utcnow()
+
+    if not users.find_one({'user_id': userid}) is None:
+        response = users.find_one({'user_id': userid})
+
+        if response:
+            if bcrypt.check_password_hash(response['password'], password):
+                access_token = create_access_token(
+                    identity={'user_id': response['user_id']})
+                return jsonify({'token': access_token})
+            else:
+                return jsonify({'error': 'invalid username and password'})
+        else:
+            return jsonify({'result': 'no result found'})
+
+    id = users.insert({
+        'user_id': userid,
+        'password': password_bcrypt,
+        'created': created
+    })
+
+    new_user = users.find_one({'_id': id})
+    access_token = create_access_token(
+        identity={'user_id': new_user['user_id']})
+    return jsonify({'token': access_token})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
